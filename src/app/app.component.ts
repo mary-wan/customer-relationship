fc4ad5ac412b9bd2711a08d7ad010b441c89c408
mary-wan <njengamaria29@gmail.com>
2022-06-07 12:11:12 +0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'ken20956-kcb-customer-relationship-management';
+}
